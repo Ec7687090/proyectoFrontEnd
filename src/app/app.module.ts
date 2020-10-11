4ad59db4707733d67e86578e8858b4c64885545d
Ec7687090 <edcorrales1@poligran.edu.co>
2020-10-11 12:22:14 -0500

    Importamos HttpClienteModulo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FiltroComponent } from './componet/filtro/filtro.component';
 import { MostrarComponent } from './mostrar/mostrar.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { MostrarComponent } from './mostrar/mostrar.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
